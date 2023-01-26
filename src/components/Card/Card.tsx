9fb8c2383a63e0cb4e8fec65fab34aa65e7890af
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-26 19:02:46 +0300

    Cart flipping added

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,11 +5,21 @@ import image from '../../assets/img/header-logo.svg'
 import style from './Card.module.scss'
 
 const Card = () => {
+	const [flip, setFlip] = React.useState(true)
+
 	return (
-		<div className={style.card}>
-			<h3 className={style.cardTitle}>Green bird</h3>
-			<div className={style.imgWrapper}>
-				<img src={image} width={150} alt='' />
+		<div
+			className={style[`${flip ? 'card' : 'flip'}`]}
+			onClick={() => setFlip(!flip)}
+		>
+			<div className={style.back}>
+				<h3 className={style.translation}>Зеленая птица</h3>
+			</div>
+			<div className={style.front}>
+				<h3 className={style.word}>Green bird</h3>
+				<div className={style.imgWrapper}>
+					<img src={image} alt='' />
+				</div>
 			</div>
 		</div>
 	)
