e1f76f82437bea4b4976e8812eef6ee83c06f67f
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-26 11:50:24 +0300

    Header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './components/Header'
 import './scss/App.scss'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<div className='container'>
+				<Header />
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import headerLogo from '../assets/img/header-logo.svg'
+
+import style from './Header.module.scss'
+
+const Header = () => {
+	return (
+		<header className={style.header}>
+			<div className={style.logo}>
+				<img src={headerLogo} width={60} alt='header logo' />
+				<h3 className={style.title}>EasyEng</h3>
+			</div>
+			<div className={style.authButtons}>
+				<button className={style.login}>Login</button>
+				<button className={style.register}>Register</button>
+			</div>
+		</header>
+	)
+}
+
+export default Header
