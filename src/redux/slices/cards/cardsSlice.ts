eadbf72e6d5ac8592c2785a29c327b8c7594515f
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-27 12:31:13 +0300

    cardsSlice extraReducers created

--- src/redux/slices/cards/cardsSlice.ts
+++ src/redux/slices/cards/cardsSlice.ts
@@ -22,7 +22,21 @@ const cardsSlice = createSlice({
 	name: 'cards',
 	initialState,
 	reducers: {},
-	extraReducers: builder => {},
+	extraReducers: builder => {
+		builder
+			.addCase(fetchCards.pending, state => {
+				state.cards.status = Status.LOADING
+				state.cards.items = []
+			})
+			.addCase(fetchCards.fulfilled, (state, action) => {
+				state.cards.items = action.payload
+				state.cards.status = Status.SUCCESS
+			})
+			.addCase(fetchCards.rejected, state => {
+				state.cards.items = []
+				state.cards.status = Status.ERROR
+			})
+	},
 })
 
 export default cardsSlice.reducer
