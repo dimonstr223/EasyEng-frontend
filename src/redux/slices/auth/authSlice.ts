ee425fcc88492c4784bece1f798a72a32b085b09
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-27 10:01:21 +0300

    fetchAuth thunk created

--- src/redux/slices/auth/authSlice.ts
+++ src/redux/slices/auth/authSlice.ts
@@ -1,4 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from '../../../axios/axios'
+
+export const fetchAuth = createAsyncThunk('auth/fethchAuth', async params => {
+	const { data } = await axios.post('/auth/login')
+	console.log(data)
+	return data
+})
 
 const initialState = {
 	status: 'loading',
