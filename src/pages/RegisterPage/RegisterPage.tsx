0a0000ac377bb60901e7fe3a64289c9c5e1ac9d4
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-27 15:07:36 +0300

    Valid registration form created

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,20 +1,50 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form'
+
 import style from './RegisterPage.module.scss'
+import { FormData } from './types'
 
 const RegisterPage: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>()
+
+	const onSubmit = handleSubmit(values => {
+		console.log(values)
+	})
 	return (
 		<div className={style.registerContainer}>
 			<h1 className={style.title}>Registration</h1>
-			<form className={style.form}>
-				<label>Username</label>
-				<input className={style.input} type='text' />
-				<label>Password</label>
-				<input className={style.input} type='password' />
+			<form className={style.form} onSubmit={onSubmit}>
+				<div className={style.inputWrapper}>
+					<label>Username</label>
+					<input
+						className={style.input}
+						style={errors.username && { border: `3px solid #ff4e4e` }}
+						type='text'
+						{...register('username', { required: true, minLength: 3 })}
+					/>
+					{errors.username && <div className={style.error}>Enter username</div>}
+				</div>
+				<div className={style.inputWrapper}>
+					<label>Password</label>
+					<input
+						className={style.input}
+						style={errors.password && { border: `3px solid #ff4e4e` }}
+						type='password'
+						{...register('password', {
+							required: true,
+							minLength: 4,
+							maxLength: 10,
+						})}
+					/>
+					{errors.password && <div className={style.error}>Enter password</div>}
+				</div>
+				<input className={style.submitButton} type='submit' value='Submit' />
 			</form>
-			<button className={style.submitButton} type='submit'>
-				Submit
-			</button>
 		</div>
 	)
 }
