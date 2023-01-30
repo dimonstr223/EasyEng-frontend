dff7120372efb0ab12ef65daf3b018b95a03fd4f
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-30 10:54:38 +0300

    control inputs

--- src/pages/CreateCardPage/CreateCardPage.tsx
+++ src/pages/CreateCardPage/CreateCardPage.tsx
@@ -1,9 +1,33 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useRef, useState } from 'react'
 import image from '../../assets/img/header-logo.svg'
 import closeIcon from '../../assets/img/close-icon.svg'
 import style from './CreateCardPage.module.scss'
 
 const CreateCardPage: FC = () => {
+	// interface IValues {
+	// 	word: string
+	// 	translation: string
+	// 	imageURL: string
+	// }
+	// const [{ word, translation, imageURL }, setValues] = useState<IValues>({
+	// 	word: '',
+	// 	translation: '',
+	// 	imageURL: '',
+	// })
+	const [word, setWord] = useState('')
+	const [translation, setTranslation] = useState('')
+	const [imageURL, setImgaeURL] = useState('')
+
+	const handleImageUpload = async (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		if (event.target.files) {
+			const formData = new FormData()
+			const file = event.target.files[0]
+			formData.append('image', file)
+		}
+	}
+
 	const addImageRef = useRef<HTMLInputElement>(null)
 
 	const onClickAddImage = (
@@ -21,11 +45,21 @@ const CreateCardPage: FC = () => {
 			<form className={style.form}>
 				<div className={style.inputWrapper}>
 					<label>Foreign word</label>
-					<input className={style.input} type='text' />
+					<input
+						className={style.input}
+						value={word}
+						onChange={e => setWord(e.target.value)}
+						type='text'
+					/>
 				</div>
 				<div className={style.inputWrapper}>
 					<label>Translation</label>
-					<input className={style.input} type='text' />
+					<input
+						className={style.input}
+						value={translation}
+						onChange={e => setTranslation(e.target.value)}
+						type='text'
+					/>
 				</div>
 				<div className={style.addImage}>
 					<button className={style.addImageButton} onClick={onClickAddImage}>
@@ -34,12 +68,19 @@ const CreateCardPage: FC = () => {
 					<input
 						className={style.addImageInput}
 						ref={addImageRef}
+						onChange={handleImageUpload}
 						type='file'
 					/>
-					<div className={style.imgWrapper}>
-						<img className={style.image} src={image} alt='Illustration' />
-						<img className={style.removeButton} src={closeIcon} alt='remove' />
-					</div>
+					{imageURL && (
+						<div className={style.imgWrapper}>
+							<img className={style.image} src={image} alt='Illustration' />
+							<img
+								className={style.removeButton}
+								src={closeIcon}
+								alt='remove'
+							/>
+						</div>
+					)}
 				</div>
 				<input className={style.submitButton} type='submit' value='Create' />
 			</form>
