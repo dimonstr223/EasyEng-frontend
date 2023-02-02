6fe0cc0e24abfd9ef750d91fe1b43f6c4ca3be30
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-02 10:32:59 +0300

    Search styled

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState } from 'react'
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react'
 import { useAppDispatch } from '../../hooks'
 import debounce from 'lodash.debounce'
 
@@ -7,11 +7,20 @@ import {
 	fetchSearch,
 } from '../../redux/cards/asyncThunks/cardsAsyncThunks'
 
+import searchIcon from '../../assets/img/search-icon.svg'
+
 import style from './Search.module.scss'
 
 const Search: FC = () => {
 	const dispatch = useAppDispatch()
 	const [searchValue, setSearchValue] = useState('')
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	const onSearchClick = () => {
+		if (inputRef.current) {
+			inputRef.current.focus()
+		}
+	}
 
 	const testDebounce = useCallback(
 		debounce(searchValue => {
@@ -33,12 +42,14 @@ const Search: FC = () => {
 	}
 
 	return (
-		<div className={style.search}>
+		<div className={style.search} onClick={onSearchClick}>
+			<img className={style.searchIcon} src={searchIcon} alt='search' />
 			<input
 				className={style.serachInput}
 				value={searchValue}
 				onChange={onSerachInputHandler}
 				type='text'
+				ref={inputRef}
 			/>
 		</div>
 	)
