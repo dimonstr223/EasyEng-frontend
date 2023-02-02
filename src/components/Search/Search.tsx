06d99a221d6de4f531456b93dd2aba6a2e0afd3d
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-02 08:27:56 +0300

    serachInput made controllable

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import style from './Search.module.scss'
 
 const Search = () => {
+	const [searchValue, setSearchValue] = useState('')
+
+	const onSerachInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(event.target.value)
+	}
+
 	return (
 		<div className={style.search}>
-			<input className={style.serachInput} type='text' />
+			<input
+				className={style.serachInput}
+				value={searchValue}
+				onChange={onSerachInputHandler}
+				type='text'
+			/>
 		</div>
 	)
 }
