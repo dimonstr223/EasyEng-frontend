47581089b7beacb14a72be00b6e3aba0bba6c3de
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-02 08:33:01 +0300

    serching implemented

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppDispatch } from '../../hooks'
+import {
+	fetchCards,
+	fetchSearch,
+} from '../../redux/cards/asyncThunks/cardsAsyncThunks'
 
 import style from './Search.module.scss'
 
 const Search = () => {
+	const dispatch = useAppDispatch()
 	const [searchValue, setSearchValue] = useState('')
+
+	useEffect(() => {
+		dispatch(fetchSearch(searchValue))
+		if (searchValue === '') {
+			dispatch(fetchCards())
+		}
+	}, [searchValue])
 
 	const onSerachInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchValue(event.target.value)
